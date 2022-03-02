bd5739305947f367cc684e135fc7f2df3bfa6ed8
Jono Hill <jono@hillnz.com>
2022-03-02 21:25:58 +1300

    try cross build

--- build.sh
+++ build.sh
@@ -9,10 +9,6 @@ cd os
 # shellcheck disable=SC2016
 sed -i 's/".*mirrors.ubuntu.com.*"/"\$MIRROR_URL"/g' etc/auto/config
 
-docker run --privileged --platform linux/arm64 -v /proc:/proc \
-    -v "$(pwd):/working_dir" \
-    -w /working_dir \
-    debian:latest \
-    ./build.sh build.conf
+sudo ./build.sh build.conf
 
 cd ..
