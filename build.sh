4b330188d7762e01c3d5d3e69420c56f32a593a0
Jono Hill <jono@hillnz.com>
2022-11-08 21:22:06 +1300

    Try removing arm packages

--- build.sh
+++ build.sh
@@ -7,7 +7,28 @@ cd os
 
 # Patch out mirror config because many don't have arm binaries
 # shellcheck disable=SC2016
-sed -i 's/".*mirrors.ubuntu.com.*"/"\$MIRROR_URL"/g' etc/auto/config
+sed -i '' 's/".*mirrors.ubuntu.com.*"/"\$MIRROR_URL"/g' etc/auto/config
+
+# Remove packages unavailable for ARM
+packages=(bcmwl-kernel-source intel-microcode iucode-tool lupin-support secureboot-db)
+pool_files=(etc/config/package-lists.default/pool.list.binary etc/config/package-lists.distinst/pool.list.binary)
+for package in "${packages[@]}"; do
+    for pool_file in "${pool_files[@]}"; do
+        sed -i '' "/^${package}/d" "${pool_file}"
+        
+        # Insert ARM-specific packages
+        cat <<EOF >> "${pool_file}"
+#if ARCHITECTURES arm64
+grub-efi-arm64
+grub-efi-arm64-bin
+grub-efi-arm64-signed
+shim
+shim-signed
+#endif
+EOF
+
+    done
+done
 
 docker run --privileged --platform linux/arm64 -v /proc:/proc \
     -v "$(pwd):/working_dir" \
