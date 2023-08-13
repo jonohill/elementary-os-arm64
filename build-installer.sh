d9c5f6a10a7aff52143280f48de6b40070c2a278
Jono Hill <jono@hillnz.com>
2023-08-13 16:57:33 +1200

    Fix apt in script

--- build-installer.sh
+++ build-installer.sh
@@ -12,10 +12,11 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 63C46DF0140D738961429F4
 add-apt-repository "deb http://apt.pop-os.org/release $(lsb_release -cs) main"
 apt-get update
 
+git config --global --add safe.directory /working_dir
 git config --global --add safe.directory /__w/installer/installer
 git checkout origin/deb-packaging -- debian
 
 sed -i 's/amd64/arm64/g' debian/control
-apt-get --no-install-recommends -qq build-dep .
+DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends build-dep .
 
 dpkg-buildpackage -us -uc
