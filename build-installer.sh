c18001c91d9d5c7e4bd8b21918bbab31851def8f
Jono Hill <jono@hillnz.com>
2023-08-13 17:21:12 +1200

    fiddle with dirs

--- build-installer.sh
+++ build-installer.sh
@@ -12,13 +12,15 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 63C46DF0140D738961429F4
 add-apt-repository "deb http://apt.pop-os.org/release $(lsb_release -cs) main"
 apt-get update
 
-git config --global --add safe.directory /working_dir
+git config --global --add safe.directory ./installer
 git config --global --add safe.directory /__w/installer/installer
-pwd
-git branch -a
+
+cd installer
 git checkout origin/deb-packaging -- debian
 
 sed -i 's/amd64/arm64/g' debian/control
 DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends build-dep .
 
 dpkg-buildpackage -us -uc
+
+cd -
